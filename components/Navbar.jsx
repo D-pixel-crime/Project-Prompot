e6f7c42e9c778a012587137cdeb3560af717f061
D-pixel-crime <dpk4383@gmail.com>
2024-03-28 10:42:21 +0530

    Navbar mounted

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,9 +3,21 @@
 import Image from "next/image";
 import Link from "next/link";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
   const userLoggedInOrNot = true;
+  const [providers, setProviders] = useState(null);
+  const [toggleDropDown, setToggleDropDown] = useState(false);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const res = await getProviders();
+      setProviders(res);
+    };
+    setProviders();
+  }, []);
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -21,10 +33,10 @@ const Navbar = () => {
       <div className="sm:flex hidden">
         {userLoggedInOrNot ? (
           <div className="flex gap-3 md:gap-5">
-            <Link href="/create-prompt" className="outline_btn">
+            <Link href="/create-prompt" className="outline_black_btn">
               Create Post
             </Link>
-            <button className="black_btn" type="button" onClick={signOut}>
+            <button className="red_btn" type="button" onClick={signOut}>
               Sign Out
             </button>
             <Link href="/profile">
@@ -38,7 +50,89 @@ const Navbar = () => {
             </Link>
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((eachProvider) => {
+                return (
+                  <button
+                    type="button"
+                    key={eachProvider.name}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      signIn(eachProvider.id);
+                    }}
+                    className="black_btn"
+                  >
+                    Sign In
+                  </button>
+                );
+              })}
+          </>
+        )}
+      </div>
+
+      <div className="sm:hidden flex relative">
+        {userLoggedInOrNot ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              className="rounded-full cursor-pointer"
+              alt="profile"
+              onClick={(e) => {
+                e.preventDefault();
+                setToggleDropDown((preValue) => !preValue);
+              }}
+            />
+            {toggleDropDown && (
+              <div className="dropdown">
+                <Link
+                  href="/"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropDown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropDown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropDown(false);
+                    signOut();
+                  }}
+                  className="red_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((eachProvider) => {
+                return (
+                  <button
+                    type="button"
+                    key={eachProvider.name}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      signIn(eachProvider.id);
+                    }}
+                    className="black_btn"
+                  >
+                    Sign In
+                  </button>
+                );
+              })}
+          </>
         )}
       </div>
     </nav>
