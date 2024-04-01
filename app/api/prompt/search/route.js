6eebf638cb32329aaf10b7878004793e4be27e83
D-pixel-crime <dpk4383@gmail.com>
2024-04-01 22:41:20 +0530

    Search using username and underscore.js library

--- app/api/prompt/search/route.js
+++ app/api/prompt/search/route.js
@@ -1,5 +1,7 @@
 import { Prompts } from "@models/prompt";
+import { User } from "@models/user";
 import { connectToDB } from "@utils/database";
+import _ from "underscore";
 
 export const POST = async (req) => {
   const { searchText } = await req.json();
@@ -14,7 +16,18 @@ export const POST = async (req) => {
       ],
     }).populate("creator");
 
-    return new Response(JSON.stringify(data1), { status: 200 });
+    const users = await User.find({ username: { $regex: searchText } });
+
+    let data2 = [];
+
+    for (let i of users) {
+      let temp = await Prompts.find({ creator: i._id }).populate("creator");
+      data2 = _.union(data2, temp);
+    }
+
+    const finalData = _.union(data1, data2);
+
+    return new Response(JSON.stringify(finalData), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify(error), { status: 500 });
   }
